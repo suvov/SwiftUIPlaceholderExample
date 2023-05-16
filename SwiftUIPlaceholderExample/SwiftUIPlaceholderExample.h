d3aaa8c681a79f389578cd245c6d58a4a3c8a647
Vladimir Shutyuk <vladimir.shutyuk@osome.com>
2023-05-16 15:36:25 +0200

    Initial Commit

---
+++ SwiftUIPlaceholderExample/SwiftUIPlaceholderExample.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIPlaceholderExample.h
+//  SwiftUIPlaceholderExample
+//
+//  Created by Vladimir Shutyuk on 16.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIPlaceholderExample.
+FOUNDATION_EXPORT double SwiftUIPlaceholderExampleVersionNumber;
+
+//! Project version string for SwiftUIPlaceholderExample.
+FOUNDATION_EXPORT const unsigned char SwiftUIPlaceholderExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIPlaceholderExample/PublicHeader.h>
+
+
