b7c63f5175898e475fbd4bae1115d50eb698524d
Vladimir Shutyuk <vladimir.shutyuk@osome.com>
2023-05-16 15:53:17 +0200

    Add city list view

---
+++ SwiftUIPlaceholderExample/CityListView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+struct CityListView: View {
+  // MARK: Model
+
+  struct Model {
+    let cities: [CityView.Model]
+  }
+
+  let model: Model
+
+  // MARK: View
+
+  var body: some View {
+    VStack {
+      ForEach(model.cities, id: \.self) {
+        CityView(model: $0)
+          .padding(.vertical)
+      }
+    }
+    .padding(.horizontal)
+  }
+}
+
+// MARK: - Preview
+
+struct CityListView_Previews: PreviewProvider {
+  static var previews: some View {
+    CityListView(model: .stub)
+      .previewLayout(.sizeThatFits)
+      .previewDisplayName("Normal")
+  }
+}
+
+// MARK: - Model stub
+
+extension CityListView.Model {
+  static var stub: Self {
+    .init(cities: [.stub, .stub, .stub])
+  }
+}
